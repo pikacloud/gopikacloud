6aa46f2993e77829f10e6316d64bc4a359b2355f
Benjamin Jorand <benjamin.jorand@gmail.com>
2018-01-08 14:54:32 +0100

    superNetwork create

--- supernetwork.go
+++ supernetwork.go
@@ -1,15 +1,18 @@
 package gopikacloud
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // SuperNetwork defines a pikacloud supernetwork
 type SuperNetwork struct {
-	Key       string `json:"key"`
+	Key       string `json:"key,omitempty"`
 	Cidr      string `json:"cidr"`
 	DNSDomain string `json:"dns_domain"`
 }
 
-// SuperNetwork returns current supernetwork
+// SuperNetwork return a supernetwork
 func (client *Client) SuperNetwork(aid string) (SuperNetwork, error) {
 	sn := SuperNetwork{}
 	uri := fmt.Sprintf("run/agents/%s/supernetwork/", aid)
@@ -17,4 +20,18 @@ func (client *Client) SuperNetwork(aid string) (SuperNetwork, error) {
 		return SuperNetwork{}, err
 	}
 	return sn, nil
+}
+
+// CreateSuperNetwork create a supernetwork
+func (client *Client) CreateSuperNetwork(supernetwork interface{}, aid string) (SuperNetwork, error) {
+	sn := SuperNetwork{}
+	uri := fmt.Sprintf("run/agents/%s/supernetwork/", aid)
+	status, err := client.Post(uri, supernetwork, &sn)
+	if err != nil {
+		return SuperNetwork{}, err
+	}
+	if status == 201 || status == 200 {
+		return sn, nil
+	}
+	return sn, errors.New("Failed to create superNetwork")
 }
--- supernetwork_test.go
+++ supernetwork_test.go
@@ -27,3 +27,30 @@ func TestSuperNetwork_SuperNetwork(t *testing.T) {
 		t.Errorf("SuperNetwork returned %+v, want %+v", sn, want)
 	}
 }
+
+func TestSuperNetwork_Create(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/run/agents/foobar/supernetwork/", func(w http.ResponseWriter, r *http.Request) {
+		want := map[string]interface{}{"cidr": "10.42.0.0/16", "dns_domain": "foo.bar."}
+
+		testMethod(t, r, "POST")
+		w.WriteHeader(http.StatusCreated)
+		testRequestJSON(t, r, want)
+
+		fmt.Fprintf(w, `{"cidr": "10.42.0.0/16", "dns_domain": "foo.bar.", "key": "a"}`)
+	})
+
+	superNetwork := SuperNetwork{Cidr: "10.42.0.0/16", DNSDomain: "foo.bar."}
+	res, err := client.CreateSuperNetwork(superNetwork, "foobar")
+
+	if err != nil {
+		t.Errorf("Create superNetwork returned error: %v", err)
+	}
+
+	want := SuperNetwork{Cidr: "10.42.0.0/16", DNSDomain: "foo.bar.", Key: "a"}
+	if !reflect.DeepEqual(res, want) {
+		t.Errorf("Create superNetwork returned %+v, want %+v", res, want)
+	}
+}
